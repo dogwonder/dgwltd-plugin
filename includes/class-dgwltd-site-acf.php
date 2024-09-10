30524a3f42778047ad0dc38e8812b09f2782ba03
Rich Holman <richholman@dogwonderPro.communityfibre.co.uk>
2024-09-10 18:26:16 +0100

    Add cards

--- includes/class-dgwltd-site-acf.php
+++ includes/class-dgwltd-site-acf.php
@@ -25,8 +25,8 @@ class Dgwltd_Site_ACF {
 		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/anchor/block.json' );
 		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/breadcrumbs/block.json' );
 		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/call-to-action/block.json' );
-		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . '/build/cards/block.json' );
-    	register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . '/build/cards/query/block.json' );
+		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/cards/block.json' );
+    	register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/cards/query/block.json' );
 		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/embed/block.json' );
 		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/feature/block.json' );
 		register_block_type( DGWLTD_SITE_PLUGIN_BLOCKS . 'build/hero/block.json' );
