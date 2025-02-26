1d41a0de917806a029c8256176dbec716f8707d8
Rich Holman <richholman@dogwonderPro.communityfibre.co.uk>
2025-02-26 12:45:23 +0000

    Add focal point

--- src/blocks/build/hero/hero-edit.asset.php
+++ src/blocks/build/hero/hero-edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-i18n'), 'version' => 'aec54284dbe93c575561');
+<?php return array('dependencies' => array('wp-data', 'wp-i18n'), 'version' => 'ef5b531123a768c86cbe');
